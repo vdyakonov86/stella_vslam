61c2aecf262845fbc2890f2cc62b1fa0d2aeffc2
Vadim Dyakonov <vdyakonov86@yandex.ru>
2025-05-06 23:32:25 +0300

    Modify build scripts for turning on stack trace

--- scripts/docker/build_stella_vslam.bash
+++ scripts/docker/build_stella_vslam.bash
@@ -1,10 +1,17 @@
-NUM_THREADS="$(expr $(nproc) - 1)"
+#!/bin/bash
 
-cd /stella_vslam && set -x && \
+NUM_THREADS="$(expr $(nproc) - 1)"
+# cd /stella_vslam && \
+#   cd build && \
+#   sudo cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .. && \
+#   sudo make -j4 && \
+#   sudo make install
+
+cd /stella_vslam && \
   cd build && \
-  sudo cmake \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    .. && \
-  sudo make -j$NUM_THREADS && \
+  sudo cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -g2 -O0" \
+      -DUSE_BACKTRACE=ON \
+      -DENABLE_STACKTRACE=ON .. && \
+  sudo make -j4 && \
   sudo make install
-  
--- scripts/docker/build_stella_vslam_examples.bash
+++ scripts/docker/build_stella_vslam_examples.bash
@@ -1,10 +1,17 @@
+#!/bin/bash
+
 NUM_THREADS="$(expr $(nproc) - 1)"
+
+# cd /stella_vslam/stella_vslam_examples && \
+#   cd build && \
+#   sudo cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DUSE_STACK_TRACE_LOGGER=ON .. && \
+#   sudo make -j4
 
 cd /stella_vslam/stella_vslam_examples && \
   cd build && \
-  sudo cmake \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} \
-    -DUSE_STACK_TRACE_LOGGER=ON \
-    .. && \
-  sudo make -j$NUM_THREADS
+  sudo cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -g2 -O0" \
+      -DUSE_BACKTRACE=ON \
+      -DENABLE_STACKTRACE=ON .. && \
+  sudo make -j4
+  
